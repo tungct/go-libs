1970d1a6f50bbfc527d7286b23d9ac1642a171ff
tungct <caotungjs@gmail.com>
2018-02-07 10:43:49 +0700

    test socket

---
+++ test/server.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	messageInitResponse       = "OK"
+)
+
+func SocketServer(port int) {
+
+	listen, err := net.Listen("tcp4", ":"+strconv.Itoa(port))
+	defer listen.Close()
+	if err != nil {
+		log.Fatalf("Socket listen port %d failed,%s", port, err)
+		os.Exit(1)
+	}
+	log.Printf("Begin listen port: %d", port)
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatalln(err)
+			continue
+		}
+		go handler(conn)
+	}
+
+}
+
+func handler(conn net.Conn) {
+
+	defer conn.Close()
+
+	var (
+		buf = make([]byte, 1024)
+		r   = bufio.NewReader(conn)
+		w   = bufio.NewWriter(conn)
+	)
+
+	LOOP:
+	for {
+		n, err := r.Read(buf)
+		data := string(buf[:n])
+
+		switch err {
+		case io.EOF:
+			break LOOP
+		case nil:
+			log.Println("Receive:", data)
+			if isTransportOver(data) {
+				break LOOP
+			}
+
+		default:
+			log.Fatalf("Receive data failed:%s", err)
+			return
+		}
+
+	}
+	w.Write([]byte(messageInitResponse))
+	w.Flush()
+	log.Printf("Send: %s", messageInitResponse)
+
+}
+
+func isTransportOver(data string) (over bool) {
+	over = strings.HasSuffix(data, "\r\n\r\n")
+	return
+}
+
+func main() {
+
+	port := 8000
+
+	SocketServer(port)
+
+}
